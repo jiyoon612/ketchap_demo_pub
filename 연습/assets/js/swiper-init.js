ed91abf9ac82dbf85fbce64aef1faa0b0a2545ad
jiyoon612 <wldbsvk612@gmail.com>
2024-11-25 16:59:42 +0900

    헤더와 스와이퍼 추가로 UI/UX 개선 - importHeader.js로 재사용 가능한 헤더 구현 - Swiper.js를 사용하여 동적 캐러셀 추가 - 스타일 추가로 디자인 통일성 강화

---
+++ 연습/assets/js/swiper-init.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const swiper = new Swiper('.swiper-container', {
+        loop: true, 
+        autoplay: {
+            delay: 3000, 
+            disableOnInteraction: false, 
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true, 
+        },
+    });
+});

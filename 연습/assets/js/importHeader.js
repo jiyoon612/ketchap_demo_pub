ce00efafa724264e25d373d400e98facb13b9aa6
jiyoon612 <wldbsvk612@gmail.com>
2024-11-26 17:35:10 +0900

    메인 페이지 추가 및 footer.html 추가 1. 메인 페이지 컨텐츠 (실시간 인기 강의, 인기 VOD 강의) 추가 2. footer.html 추가 및 연동 3. submenu 추가 ( 스타일 미완성 )

--- 연습/assets/js/importHeader.js
+++ 연습/assets/js/importHeader.js
@@ -12,4 +12,10 @@ document.addEventListener("DOMContentLoaded", function () {
         .catch((error) => {
             console.error("Error loading header:", error);
         });
+        fetch('footer.html')
+        .then((response) => response.text())
+        .then((html) => {
+          document.getElementById('footer-container').innerHTML = html;
+        })
+        .catch((error) => console.error('Footer 로드 에러:', error));
 });
